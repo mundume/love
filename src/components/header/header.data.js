bbf29c60640ed1a7c6b68ec48455c21dc9d308d0
nzaih1999 <nzaih18@gmail.com>
2021-11-09 21:23:15 +0300

    first commit

---
+++ src/components/header/header.data.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    path: 'home',
+    label: 'Home',
+  },
+  {
+    path: 'feature',
+    label: 'Services',
+  },
+  {
+    path: 'pricing',
+    label: 'Pricing',
+  },
+  {
+    path: 'testimonial',
+    label: 'Testimonial',
+  },
+
+  {
+    path:'portfolio',
+    label:'Portfolio',
+  }
+];
+
